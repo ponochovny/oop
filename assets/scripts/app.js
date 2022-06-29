4dcba66823c292f59954ffd9d1d9b0dff376fea2
ponochovny <ponochovny@gmail.com>
2022-06-29 04:44:37 +0300

    added basic classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,42 +1,110 @@
-const productList = {
-	products: [
-		{
-			title: 'A Pillow',
-			imageUrl:
-				'https://www.maxpixel.net/static/photo/1x/Sofa-Living-Room-Real-Estate-Table-Picture-6893109.jpg',
-			price: 19.99,
-			description: 'A soft pillow!',
-		},
-		{
-			title: 'A Carpet',
-			imageUrl:
-				'https://www.maxpixel.net/static/photo/1x/Colors-Clothing-Fabric-Rugs-Material-Carpet-6782082.jpg',
-			price: 89.99,
-			description: 'A carpet which you might like - or not.',
-		},
-	],
+class Product {
+	// title = 'DEFAULT'
+	// imageUrl
+	// description
+	// price
+
+	constructor(title, image, desc, price) {
+		this.title = title
+		this.imageUrl = image
+		this.description = desc
+		this.price = price
+	}
+}
+
+class ProductList {
+	products = [
+		new Product(
+			'A Pillow',
+			'https://www.maxpixel.net/static/photo/1x/Sofa-Living-Room-Real-Estate-Table-Picture-6893109.jpg',
+			'A soft pillow!',
+			19.99
+		),
+		new Product(
+			'A Carpet',
+			'https://www.maxpixel.net/static/photo/1x/Colors-Clothing-Fabric-Rugs-Material-Carpet-6782082.jpg',
+			'A carpet which you might like - or not.',
+			89.99
+		),
+	]
+
+	constructor() {}
+
 	render() {
-		const renderHook = document.getElementById('app')
 		const prodList = document.createElement('ul')
 		prodList.className = 'product-list'
 		for (const prod of this.products) {
-			const prodEl = document.createElement('li')
-			prodEl.className = 'product-item'
-			prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}" />
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `
+			const productItem = new ProductItem(prod)
+			const prodEl = productItem.render()
 			prodList.append(prodEl)
 		}
-		renderHook.append(prodList)
-	},
+		return prodList
+	}
 }
 
-productList.render()
+class ShoppingCart {
+	items = []
+
+	addProduct(product) {
+		this.items.push(product)
+		this.totalOutput = `<h2>Total: \$${1}</h2>`
+	}
+
+	render() {
+		const cartEl = document.createElement('section')
+		cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `
+		cartEl.className = 'cart'
+		this.totalOutput = cartEl.querySelector('h2')
+		return cartEl
+	}
+}
+
+class ProductItem {
+	constructor(product) {
+		this.product = product
+	}
+
+	addToCart() {
+		console.log('Adding product to cart...')
+		console.log(this.product)
+	}
+
+	render() {
+		const prodEl = document.createElement('li')
+		prodEl.className = 'product-item'
+		prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" />
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    `
+		const addCartButton = prodEl.querySelector('button')
+		addCartButton.addEventListener('click', this.addToCart.bind(this))
+		return prodEl
+	}
+}
+
+class Shop {
+	render() {
+		const renderHook = document.getElementById('app')
+
+		const cart = new ShoppingCart()
+		const cartEl = cart.render()
+		const productList = new ProductList()
+		const prodListEl = productList.render()
+
+		renderHook.append(cartEl)
+		renderHook.append(prodListEl)
+	}
+}
+
+const shop = new Shop()
+shop.render()
